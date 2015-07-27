fd99e6aa6cb24c6af33138b3e1a62099571e9e8c
singhmanu <manusingh92@gmail.com>
2015-07-26 21:53:37 -0400

    Added start to go to user location or default location at the start. Improved Map interface by enabling/disabling map features.

--- Challenge/app/src/main/java/com/example/manu/challenge/MapsActivity.java
+++ Challenge/app/src/main/java/com/example/manu/challenge/MapsActivity.java
@@ -1,22 +1,103 @@
 package com.example.manu.challenge;
 
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    //SharedPreferences mySettings = getSharedPreferences("MyPrefsFile", 0);
+    private double savedLat = 37.774929;
+    private double savedLng = -122.419416;
+    private float savedZoom = 4f;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
+        // Gets previous latitude and Longitude values saved in the app
+/*        if(mySettings.contains("zoom"))
+        {
+            savedZoom = mySettings.getFloat("zoom", 4);
+            savedLng = (double) mySettings.getFloat("longitude", -122.419416f);
+            savedLat = (double) mySettings.getFloat("latitude", 37.774929f);
+        }*/
+
+        // Settings of Map
+        mMap.getUiSettings().setRotateGesturesEnabled(false);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(false);
+        mMap.getUiSettings().setRotateGesturesEnabled(false);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        //mMap.getUiSettings().setIndoorLevelPickerEnabled(false);
+
+        // Locates location of user
+        Location myLocation = null;
+        boolean er = false;
+
+        try{
+            // tries to find my location
+            LocationManager myLocManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            if(myLocManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+            {
+                myLocation = mMap.getMyLocation();
+            } else
+            {
+                er = true;
+            }
+
+        } catch(Exception e)
+        {
+            er = true;
+        }
+        LatLng myLatLng;
+        if (er) {
+            // sets a default location for the coordinates
+            myLatLng = new LatLng(37.774929, -122.419416);
+        } else {
+            myLatLng = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+        }
+
+        // zooms map to designated location
+        zoomMap(myLatLng, 1);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        // Save last position of map
+       /* SharedPreferences.Editor editor = mySettings.edit();
+        CameraPosition camFinal = mMap.getCameraPosition();
+        editor.putFloat("zoom", camFinal.zoom);
+        editor.putFloat("latitude", (float)mMap.getMyLocation().getLatitude());
+        editor.putFloat("longitude", (float)mMap.getMyLocation().getLongitude());
+        editor.commit();*/
+    }
+
+    private void zoomMap(LatLng myLatLng, float zoomVal)
+    {
+        if(mMap == null) {
+            return;
+        }
+
+        if(zoomVal == 0)
+        {
+            zoomVal = 6f;
+        }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatLng, zoomVal));
     }
 
     @Override
@@ -25,41 +106,12 @@ public class MapsActivity extends FragmentActivity {
         setUpMapIfNeeded();
     }
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.activity_maps))
                     .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
         }
-    }
-
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 }
